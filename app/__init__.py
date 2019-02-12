e585ffe7943f4064418cf0784a6ffcb689e997f0
rai <richard.iserman@gmail.com>
2019-02-12 15:12:39 -0500

    This commit adds app files too. Still need to prune some of these back. All very basic.

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app import routes
