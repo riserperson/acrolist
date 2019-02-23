9b15138c886b2c0ebd524ff4e393385d291fd168
riserperson <40473882+riserperson@users.noreply.github.com>
2019-02-23 13:08:46 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ozymandias'
-    UPLOAD_FOLDER = os.getcwd() + '/uploads/'
-    ALLOWED_EXTENSIONS = set(("doc","pdf","csv","txt"))
